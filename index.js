aae80e0d77b9b35af98d6c88e9c9ed33edb34946
Ngozi Okafor <ngoziokafor2014@gmail.com>
2022-01-12 14:09:43 +0100

    create a file

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const a = 1;
-console.log(a);
+import chalk from 'chalk';
+
+function generateRandomColor() {
+  const randomNumbers = '0123456789abcdef';
+
+  let randomColor = '#';
+  for (let i = 0; i < 6; i++) {
+    const randomHexColor = Math.floor(Math.random() * randomNumbers.length);
+    randomColor += randomNumbers[randomHexColor];
+  }
+  return randomColor;
+}
+const hexColor = generateRandomColor();
+
+const colorBox = `###############################
+###############################
+###############################
+######                    #####
+######   (${hexColor})        #####
+######                    #####
+###############################
+###############################
+###############################`;
+
+console.log(chalk.hex(hexColor)(colorBox));
